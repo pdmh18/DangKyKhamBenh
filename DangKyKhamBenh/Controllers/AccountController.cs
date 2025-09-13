061478649d8dfe8679a02d69d9ed6f675bab4541
PhamHieu18 <phamdinhminhhieu.1810@gmail.com>
2025-09-14 00:04:50 +0700

    update code

--- DangKyKhamBenh/Controllers/AccountController.cs
+++ DangKyKhamBenh/Controllers/AccountController.cs
@@ -1,18 +1,63 @@
-﻿using System;
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI.WebControls.WebParts;
 
 namespace DangKyKhamBenh.Controllers
 {
     public class AccountController : Controller
     {
         // GET: Account
+        [HttpGet]
         public ActionResult Login()
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Login(string user, string password)
+        {
+            bool isValid = false;
+            System.Diagnostics.Debug.WriteLine($"[DEBUG] User: '{user}', Password: '{password}'");
+            using (var conn = new OracleConnection(
+                ConfigurationManager.ConnectionStrings["OracleDbContext"].ConnectionString))
+            {
+                conn.Open();
+                string sql = @"SELECT COUNT(*) 
+               FROM TAIKHOAN 
+               WHERE TRIM(UPPER(TK_UserName)) = TRIM(UPPER(:pUser)) 
+                 AND TRIM(TK_PassWord) = TRIM(:pPassword)";
+
+
+                using (var cmd = new OracleCommand(sql, conn))
+                {
+                    cmd.BindByName = true;
+                    cmd.Parameters.Add(new OracleParameter("pUser", user?.Trim()));
+                    cmd.Parameters.Add(new OracleParameter("pPassword", password?.Trim()));
+
+                    int count = Convert.ToInt32(cmd.ExecuteScalar());
+                    isValid = (count > 0);
+                }
+            }
+
+            if (isValid)
+            {
+                Session["User"] = user;
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.Error = $"Invalid username or password. (User={user}, Pass={password})";
+                return View();
+            }
+        }
+
+
+
 
         public ActionResult Register()
         {
