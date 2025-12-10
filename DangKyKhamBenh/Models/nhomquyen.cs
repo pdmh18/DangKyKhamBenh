1b27174db7ee2edce18c5c4f0ce79080a7b22abb
PhamHieu18 <phamdinhminhhieu.1810@gmail.com>
2025-12-10 15:45:10 +0700

    update project

--- DangKyKhamBenh/Models/nhomquyen.cs
+++ DangKyKhamBenh/Models/nhomquyen.cs
@@ -5,7 +5,24 @@ using System.Web;
 
 namespace DangKyKhamBenh.Models
 {
-    public class nhomquyen
+    public class Role
     {
+        public int RoleID { get; set; }
+        public string RoleName { get; set; }
+        public List<Permission> Permissions { get; set; }
     }
+
+    public class Permission
+    {
+        public int PermissionID { get; set; }
+        public string PermissionName { get; set; }
+    }
+
+    public class UserRole
+    {
+        public int UserID { get; set; }
+        public string Username { get; set; }
+        public Role Role { get; set; } 
+    }
+
 }
